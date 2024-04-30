b411bdeb5c9eb2968ad9bcfe5586d51fe0ffd391
ברינה איתמר <126776819+itamarbrina@users.noreply.github.com>
2024-04-30 14:01:33 +0300

    Update and rename start_container to start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,3 +1,2 @@
 #!bin/bash
-docker pull itamarbrina/final-python:10-0b8db9659339ceabf2ffb68649fadb957276aa2f
 docker run -itd -p 80:5000 --name ex3_contaniner itamarbrina/final-python:10-0b8db9659339ceabf2ffb68649fadb957276aa2f
